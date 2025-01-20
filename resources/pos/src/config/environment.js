f11f904769ac77c59a7f675153f059f5a516e0ef
Mohamed Soueilem <mlsoueilem@gmail.com>
2025-01-20 04:10:29 -0500

    fixing env

--- resources/pos/src/config/environment.js
+++ resources/pos/src/config/environment.js
@@ -1,3 +1,11 @@
+// console.log('Debugin environment configuration loaded.');
+// console.log("Current URL:", window.location.href);
+// console.log("Protocol: " + window.location.protocol);
+// console.log("Hostname: " + window.location.hostname);
+// console.log("Port: " + window.location.port);
+// console.log("Pathname: " + window.location.pathname);
+// console.log("Search: " + window.location.search);
+// console.log("Hash: " + window.location.hash);
 export const environment = {
-    URL: window.location.protocol + '//' + window.location.hostname,
+    URL: window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port : '') ,
 };
